a0a2e3b19f4e22a5a2eee7401fcd8a35fb1fe2c2
Peter Kaufman <peterkaufmanenator@gmail.com>
2017-08-19 17:29:26 +0900

    Adding in the ability to delete a city from the list and adding in a small description.

--- src/js/weatherizr/WeatherizrCity.jsx
+++ src/js/weatherizr/WeatherizrCity.jsx
@@ -1,17 +1,66 @@
 import React from 'react';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 
 //Renders the weather for one city.
 export default class WeatherizrCity extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+    this.openDeletePopup = this.openDeletePopup.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.deleteEntry = this.deleteEntry.bind(this);
+  }
+
+  openDeletePopup() {
+    this.setState({open: true});
+  }
+
+  handleClose() {
+    this.setState({open: false});
+  }
+
+  deleteEntry() {
+    this.setState({open: false});
+    this.props.onDeleteCity(this.props.index);
+  }
+
   render() {
+
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        onClick={this.handleClose}
+      />,
+      <FlatButton
+        label="Remove"
+        secondary={true}
+        color="red500"
+        onClick={this.deleteEntry}
+      />,
+    ];
+
     return <div className="weatherizr-city">
       <span className="city-name">{this.props.city.name}</span>
       <div className="weather-entries">
         {this.props.city.weather.slice(0, 5).map((entry, index) => {
-          console.log(entry);
-          return <WeatherEntry data={entry} />
+          return <WeatherEntry data={entry} key={index} />
         })}
       </div>
+      <span className="delete" onClick={this.openDeletePopup}><i className="fa fa-times" aria-hidden="true"></i></span>
+
+      <Dialog
+        title="Remove City"
+        actions={actions}
+        modal={true}
+        open={this.state.open}>
+        Are you sure you wish to remove this city?
+      </Dialog>
+
     </div>
   }
 
